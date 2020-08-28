22e0de4069ea60b49c80591cee92763c4af99ad8
Den <den.polusmyak@gmail.com>
2020-08-29 00:19:52 +0300

    Initial

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div>
+      <h1 style={{ textAlign: "center" }}>Welcome to my WIP portfolio</h1>
+    </div>
+  </div>
+);
+
+export default Home;
